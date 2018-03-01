fbf6a2f346da3bf5d717ec82b906c8d1d955fce6
FABIO GURGEL <fabio.gurgel@pge.ce.gov.br>
2018-03-01 14:51:08 -0300

    add user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
+  root 'user#index'
+  get 'user/index'
+
+  get 'user/new'
+
+  get 'user/create'
+
+  get 'user/update'
+
+  get 'user/edit'
+
+  get 'user/delete'
+
+  get 'user/show'
+
   get 'bemvindo/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
